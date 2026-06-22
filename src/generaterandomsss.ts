d5623e1cfdf8074c2742e101c1d75a20e91df5f7
kon2raya24 <konturaya@gmail.com>
2026-06-22 09:01:53 +0000

    docs: add JSDoc comments to generaterandomsss.ts

--- src/generaterandomsss.ts
+++ src/generaterandomsss.ts
@@ -1,3 +1,6 @@
+/**
+ * generateRandomSSS
+ */
 export function generateRandomSSS(): string {
   const sss = Array.from({length: 10}, () => Math.floor(Math.random() * 10)).join("");
   return formatSSS(sss);
